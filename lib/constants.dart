657b525d0a4a22ff5a0e3df9fd9112d3000bf676
unknown <shubham.tade@gmail.com>
2022-03-19 00:48:00 +0530

    added some new dart file

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHight = 80.0;
+const kActiveContainerColor = Color(0xFF1D1E33);
+const kInactiveContainerColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  color: Color(0xFF8D8E98),
+  fontWeight: FontWeight.w900,
+);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,40 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reuseble_Card.dart';
-
-const bottomContainerHight = 80.0;
-const activeContainerColor = Color(0xFF1D1E33);
-const inactiveContainerColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
+import 'constants.dart';
 
 enum Gender {
   male,
   female,
 }
 
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
 class _InputPageState extends State<InputPage> {
-  Color maleCardColor = inactiveContainerColor;
-  Color femaleCardColor = inactiveContainerColor;
+  Color maleCardColor = kInactiveContainerColor;
+  Color femaleCardColor = kInactiveContainerColor;
+
+  int height = 180;
 
   void updateCardColor(Gender selectGender) {
     if (selectGender == Gender.male) {
-      if (maleCardColor == inactiveContainerColor) {
-        maleCardColor = activeContainerColor;
-        femaleCardColor = inactiveContainerColor;
+      if (maleCardColor == kInactiveContainerColor) {
+        maleCardColor = kActiveContainerColor;
+        femaleCardColor = kInactiveContainerColor;
       } else {
-        maleCardColor = inactiveContainerColor;
+        maleCardColor = kInactiveContainerColor;
       }
     } else {
-      if (femaleCardColor == inactiveContainerColor) {
-        femaleCardColor = activeContainerColor;
-        maleCardColor = inactiveContainerColor;
+      if (femaleCardColor == kInactiveContainerColor) {
+        femaleCardColor = kActiveContainerColor;
+        maleCardColor = kInactiveContainerColor;
       } else {
-        femaleCardColor = inactiveContainerColor;
+        femaleCardColor = kInactiveContainerColor;
       }
     }
   }
@@ -50,6 +48,7 @@ class _InputPageState extends State<InputPage> {
         ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
@@ -133,32 +132,108 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
+
+          // MIDDLE
           Expanded(
-            child: ReusebleCard(
-              colour: activeContainerColor,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'HIEGHT',
+                    style: kLabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
+                  ),
+                  // SliderTheme(
+                  //   data: SliderThemeData(
+                  //     thumbShape: RoundRangeSliderThumbShape(
+                  //       enabledThumbRadius: 15.0,
+                  //     ),
+                  //   ),
+                  //   child: Slider(
+                  //     value: height.toDouble(),
+                  //     onChanged: (double newValue) {
+                  //       setState(
+                  //         () {
+                  //           height = newValue.toInt();
+                  //         },
+                  //       );
+                  //     },
+                  //     thumbColor: Color(0xFFEB1555),
+                  //     activeColor: Color(0xFFFFFFFF),
+                  //     inactiveColor: Color(0xFF8D8E98),
+                  //     min: 120,
+                  //     max: 220,
+                  //   ),
+                  // ),
+
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color(0xFFEB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
             ),
           ),
+
+          // Final Cards
           Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: ReusebleCard(
-                    colour: activeContainerColor,
+                    colour: kActiveContainerColor,
                   ),
                 ),
                 Expanded(
                   child: ReusebleCard(
-                    colour: activeContainerColor,
+                    colour: kActiveContainerColor,
                   ),
                 )
               ],
             ),
           ),
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: EdgeInsets.only(top: 10),
             width: double.infinity,
-            height: bottomContainerHight,
+            height: kBottomContainerHight,
           ),
         ],
       ),
